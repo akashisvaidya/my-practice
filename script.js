998235fb1ef3f405fb5f9da21fea7a55a0ca64d5
“Aakash <akashisvaidya@gmail.com>
2022-10-28 20:46:09 +1100

    few more pracitce

--- script.js
+++ script.js
@@ -221,19 +221,34 @@ followingDay();
 // find the length of the array and if the day is the last value in the array then return array[0]
 */
 
-let nb1 = Number(prompt("Enter nb1:"));
-let nb2 = Number(prompt("Enter nb2:"));
-let nb3 = Number(prompt("Enter nb3:"));
+// let nb1 = Number(prompt("Enter nb1:"));
+// let nb2 = Number(prompt("Enter nb2:"));
+// let nb3 = Number(prompt("Enter nb3:"));
 
-if (nb1 > nb2) {
-  nb1 = nb3 * 2;
-} else {
-  nb1++;
-  if (nb2 > nb3) {
-    nb1 += nb3 * 3;
-  } else {
-    nb1 = 0;
-    nb3 = nb3 * 2 + nb2;
+// if (nb1 > nb2) {
+//   nb1 = nb3 * 2;
+// } else {
+//   nb1++;
+//   if (nb2 > nb3) {
+//     nb1 += nb3 * 3;
+//   } else {
+//     nb1 = 0;
+//     nb3 = nb3 * 2 + nb2;
+//   }
+// }
+// console.log(nb1, nb2, nb3);
+
+//Create a function that takes in a number.
+// The function should return an array that contains every number //
+// from 1 to that number as seperate elements
+
+function takeInput() {
+  const val = prompt("Please enter the number: ");
+  let arr = [];
+  for (let i = 1; i <= val; i++) {
+    arr.push(i);
   }
+  console.log(arr);
 }
-console.log(nb1, nb2, nb3);
+
+takeInput();
