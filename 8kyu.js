4ed1aaaeb15d7c2a8c8a2fa4a874afb870f2ec91
Akash Vaidya <akashisvaidya@gmail.com>
2022-12-01 18:27:14 +1100

    Quarter of the year

--- 8kyu.js
+++ 8kyu.js
@@ -1,3 +1,10 @@
+/* Description 
+
+*/
+///--mySolution--///
+//--bestPractice--//
+////---------------------------------------------------------------------------------------------------
+
 /*
 Description:
 After a hard quarter in the office you decide to get some rest on a vacation.
@@ -32,5 +39,36 @@ function discountRate(days) {
 function rentalCarCost(days) {
   return baseCost(days, 40) - discountRate(days);
 }
+
+////---------------------------------------------------------------------------------------------------
+
+/* Description 
+You are given two interior angles (in degrees) of a triangle.
+Write a function to return the 3rd.
+Note: only positive integers will be tested.
+https://en.wikipedia.org/wiki/Triangle
+
+*/
+///--mySolution--///
+function otherAngle(a, b) {
+  return 180 - (a + b);
+}
+//--bestPractice--//
+////---------------------------------------------------------------------------------------------------
+
+/* Description 
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+For example: month 2 (February), is part of the first quarter; month 6 (June),
+is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+*/
+///--mySolution--///
+const quarter = (month) => {
+  return month <= 3 ? 1 : month <= 6 ? 2 : month <= 9 ? 3 : 4;
+};
+//--bestPractice--//
+const quarterOf = (m) => Math.ceil(m / 3);
+//The Math.ceil() function always rounds up and returns the smaller integer greater than
+//or equal to a given number. e.g. Math.ceil(7.004); // 8 Math.ceil(-0.95); // -0
 
 ////---------------------------------------------------------------------------------------------------
