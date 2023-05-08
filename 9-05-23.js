6282d4eefb29874b1e014ddb545ba4c62c5c7d5f
Akash Vaidya <akashisvaidya@gmail.com>
2023-05-09 09:37:55 +1000

    like dislike with reduce

--- 9-05-23.js
+++ 9-05-23.js
@@ -5,14 +5,77 @@ function maps(x) {
 }
 
 function likeOrDislike(buttons) {
-  buttonState = [];
-  console.log(buttonState);
+  // Initialize the state variable to null
+  let state = null;
 
-  if (buttonState == null) {
-    return "Nothing";
+  // Iterate over each button in the input list
+  for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+
+    // If the button is a "Like" button
+    if (button === "Like") {
+      // If the previous state was "Dislike", update the state to "Like"
+      if (state === "Dislike") {
+        state = "Like";
+      } else {
+        // Otherwise, toggle the "Like" button on or off
+        state = state === null ? "Like" : null;
+      }
+    }
+    // If the button is a "Dislike" button
+    else if (button === "Dislike") {
+      // If the previous state was "Like", update the state to "Dislike"
+      if (state === "Like") {
+        state = "Dislike";
+      } else {
+        // Otherwise, toggle the "Dislike" button on or off
+        state = state === null ? "Dislike" : null;
+      }
+    }
   }
 
-  //   return Like || Dislike || Nothing;
+  // Return the final state
+  return state;
 }
 
-console.log(likeOrDislike());
+function likeOrDislike(buttons) {
+  let state = "Nothing"; // initializing the state to nothing, when no button is pressed
+
+  for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i]; /// iterating through each of the buttons to check which one is pressed
+
+    if (button === "Like") {
+      // when the like button is pressed check the state
+      if (state === "Dislike") {
+        // if the state is dislike then overwrite it to like
+        state = "Like";
+      } else {
+        // else is the state is nothing then make it like or if state is already like then make it nothing
+        state = state === "Nothing" ? "Like" : "Nothing";
+      }
+    }
+
+    if (button === "Dislike") {
+      // now if the dislike button is pressed then follow the similiar procedure as like
+      // check the state first
+      if (state === "Like") {
+        //update it to dislike
+        state = "Dislike";
+      } else {
+        state = state === "Nothing" ? "Dislike" : "Nothing";
+        // if state = nothing is true then update it to dislike else nothing
+        // it is working as a toggle
+      }
+    }
+  }
+
+  /// now return the state
+  return state;
+}
+
+function likeDislike(buttons) {
+  return buttons.reduce(
+    (state, button) => (state === button ? Nothing : button),
+    Nothing
+  );
+}
