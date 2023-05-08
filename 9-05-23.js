63f293942211c6966be8d11bc9657082db362937
Akash Vaidya <akashisvaidya@gmail.com>
2023-05-09 09:18:33 +1000

    like vs dislike

--- 9-05-23.js
+++ 9-05-23.js
@@ -1,5 +1,18 @@
 /// Given an array of integers, return a new array with each value doubled.
-
+console.log("hello");
 function maps(x) {
   return x.map((n) => n * 2);
 }
+
+function likeOrDislike(buttons) {
+  buttonState = [];
+  console.log(buttonState);
+
+  if (buttonState == null) {
+    return "Nothing";
+  }
+
+  //   return Like || Dislike || Nothing;
+}
+
+console.log(likeOrDislike());
