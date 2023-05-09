ddb8deaef129dd5580a061df6adadf7161a318c6
Akash Vaidya <akashisvaidya@gmail.com>
2023-05-10 09:30:37 +1000

    different language greetings

---
+++ 10-05-23.js
@@ -0,0 +1,26 @@
+function greet(language) {
+  const greetings = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso",
+  };
+  if (greetings.hasOwnProperty(language)) {
+    console.log(greetings[language]);
+  } else console.log(greeting.english);
+}
+
+console.log(greet("french"));
