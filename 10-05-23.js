9688fdb82fb70caec4dc1eeccabca507002c6fb6
Akash Vaidya <akashisvaidya@gmail.com>
2023-05-10 17:38:26 +1000

    99 bottles of beer

--- 10-05-23.js
+++ 10-05-23.js
@@ -24,3 +24,31 @@ function greet(language) {
 }
 
 console.log(greet("french"));
+
+var sing = function () {
+  let lyrics = [];
+
+  for (i = 99; i >= 3; i--) {
+    lyrics.push(`${i} bottles of beer on the wall, ${i} bottles of beer.`);
+
+    lyrics.push(
+      `Take one down and pass it around, ${i - 1} bottles of beer on the wall.`
+    );
+  }
+  lyrics.push("2 bottles of beer on the wall, 2 bottles of beer.");
+  lyrics.push(
+    "Take one down and pass it around, 1 bottle of beer on the wall."
+  );
+  lyrics.push("1 bottle of beer on the wall, 1 bottle of beer.");
+  lyrics.push(
+    "Take one down and pass it around, no more bottles of beer on the wall."
+  );
+  lyrics.push("No more bottles of beer on the wall, no more bottles of beer.");
+  lyrics.push(
+    "Go to the store and buy some more, 99 bottles of beer on the wall."
+  );
+
+  return lyrics;
+};
+
+console.log(sing());
