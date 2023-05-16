faeba5f47b5fa7fb7493ee420af0587462089a6d
Akash Vaidya <akashisvaidya@gmail.com>
2023-05-16 23:59:35 +1000

    fizz

---
+++ prep.js
@@ -0,0 +1,32 @@
+//PREP
+
+// take a numer, print form 1 to that num, if the num is div by 3 print fizz and if num div by 5 print buzz if by both print fizzbuzz
+
+//P: num, whole,  +
+//R: console
+// E:
+
+function fizzBuzz(num) {
+  //loop
+  //conditionals %3&5 , %3 , %5
+  //console.log num or fizz, biz, fizzBuzz
+  for (let i = 1; i < num; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log("fizzbuzz");
+      i++;
+    }
+    if (i % 3 == 0) {
+      console.log("fizz");
+      i++;
+    }
+    if (i % 5 == 0) {
+      console.log("buzz");
+      i++;
+    }
+    console.log(i);
+  }
+}
+
+fizzBuzz(15); //1,2,fizz,4,buzz
+// fizzBuzz(3); // 1,2, fizz
+// fizzBuzz(15); //1,2,fizz,4,buzz,fizz,7,8,fizz,buzz,11,fizz,13,14,fizbuzz
